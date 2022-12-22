29dbb70d9326c7af759e65a5af0f40290b3fd009
bahadirgezer <bahadirgezerbg@gmail.com>
2022-12-22 22:18:03 +0000

    Could not finish prelab. Might look at it later.

--- block-matrix/main.py
+++ block-matrix/main.py
@@ -51,13 +51,144 @@ rnd_vals2 = np.random.rand(N, N)
 
 # WRITE YOUR CODE BELOW #
 
+def convert_index(index_i, index_j, N, K) -> tuple: 
+    # convert row-major index to block-major index
+    # block major index is the index of the element in the block-major array
+    elem = index_i * N + index_j
+    block = elem // (K * K)
+    block_i = block // (N // K)
+    block_j = block % (N // K)
+    within_block = elem % (K * K)
+    within_block_i = within_block // K
+    within_block_j = within_block % K
+    normal_i = within_block_i + block_i * K
+    normal_j = within_block_j + block_j * K
+    return (normal_i, normal_j)
+
+
+def rec(low_i, high_i, low_j, high_j, A, B, C):
+    if low_i == high_i and low_j == high_j:
+        C[low_i][low_j] = A[low_i][low_j] * B[low_i][low_j]
+    elif low_i == high_i:
+        mid_j = (low_j + high_j) // 2
+        rec(low_i, high_i, low_j, mid_j, A, B, C)
+        rec(low_i, high_i, mid_j+1, high_j, A, B, C)
+        C[low_i][low_j] = C[low_i][low_j] + C[low_i][mid_j+1]
+    elif low_j == high_j:
+        mid_i = (low_i + high_i) // 2
+        rec(low_i, mid_i, low_j, high_j, A, B, C)
+        rec(mid_i+1, high_i, low_j, high_j, A, B, C)
+        C[low_i][low_j] = C[low_i][low_j] + C[mid_i+1][low_j]
+    else:
+        mid_i = (low_i + high_i) // 2
+        mid_j = (low_j + high_j) // 2
+        rec(low_i, mid_i, low_j, mid_j, A, B, C)
+        rec(low_i, mid_i, mid_j+1, high_j, A, B, C)
+        rec(mid_i+1, high_i, low_j, mid_j, A, B, C)
+        rec(mid_i+1, high_i, mid_j+1, high_j, A, B, C)
+        C[low_i][low_j] = C[low_i][low_j] + C[mid_i+1][mid_j+1]
+    return C
+
+
+def rec32(low_i, high_i, low_j, high_j, A, B, C, offset_A, offset_B, offset_C, cs1):
+    mid_i = (low_i + high_i) // 2
+    mid_j = (low_j + high_j) // 2
+    if high_i - low_i == 1:
+        cs1.load(offset_A + low_i * N + low_j)
+        cs1.load(offset_A + low_i * N + high_j)
+        cs1.load(offset_A + high_i * N + low_j)
+        cs1.load(offset_A + high_i * N + high_j)
+        cs1.load(offset_B + low_i * N + low_j)
+        cs1.load(offset_B + low_i * N + high_j)
+        cs1.load(offset_B + high_i * N + low_j)
+        cs1.load(offset_B + high_i * N + high_j)
+        cs1.store(offset_C + low_i * N + low_j)
+        cs1.store(offset_C + low_i * N + high_j)
+        cs1.store(offset_C + high_i * N + low_j)
+        cs1.store(offset_C + high_i * N + high_j)
+        A_00 = A[low_i][low_j]
+        A_01 = A[low_i][high_j]
+        A_10 = A[high_i][low_j]
+        A_11 = A[high_i][high_j]
+        B_00 = B[low_i][low_j]
+        B_01 = B[low_i][high_j]
+        B_10 = B[high_i][low_j]
+        B_11 = B[high_i][high_j]
+        C_00 = A_00 * B_00 + A_01 * B_10
+        C_01 = A_00 * B_01 + A_01 * B_11
+        C_10 = A_10 * B_00 + A_11 * B_10
+        C_11 = A_10 * B_01 + A_11 * B_11
+        return [[C_00, C_01], [C_10, C_11]]
+
+    C_00 = rec31(low_i, mid_i, low_j, mid_j, A, B, C)
+    C_01 = rec31(low_i, mid_i, mid_j + 1, high_j, A, B, C)
+    C_10 = rec31(mid_i + 1, high_i, low_j, mid_j, A, B, C)
+    C_11 = rec31(mid_i + 1, high_i, mid_j + 1, high_j, A, B, C)
+
+    return merge(C_00, C_01, C_10, C_11)
+    
+
+
+# merge 4 sub matrices into one matrix, each is of size N/2 * N/2
+def merge(C_00, C_01, C_10, C_11):
+    C = [[0 for _ in range(N)] for _ in range(N)]
+    mid_i = N // 2
+    mid_j = N // 2
+    for i in range(mid_i):
+        for j in range(mid_j):
+            C[i][j] = C_00[i][j]
+            C[i][j + mid_j] = C_01[i][j]
+            C[i + mid_i][j] = C_10[i][j]
+            C[i + mid_i][j + mid_j] = C_11[i][j]
+    return C
+
+
+
+def rec31(low_i, high_i, low_j, high_j, A, B, C):
+    mid_i = (low_i + high_i) // 2
+    mid_j = (low_j + high_j) // 2
+    if high_i - low_i == 1:
+        A_00 = A[low_i][low_j]
+        A_01 = A[low_i][high_j]
+        A_10 = A[high_i][low_j]
+        A_11 = A[high_i][high_j]
+        B_00 = B[low_i][low_j]
+        B_01 = B[low_i][high_j]
+        B_10 = B[high_i][low_j]
+        B_11 = B[high_i][high_j]
+        C_00 = A_00 * B_00 + A_01 * B_10
+        C_01 = A_00 * B_01 + A_01 * B_11
+        C_10 = A_10 * B_00 + A_11 * B_10
+        C_11 = A_10 * B_01 + A_11 * B_11
+        return [[C_00, C_01], [C_10, C_11]]
+
+    C_00 = rec31(low_i, mid_i, low_j, mid_j, A, B, C)
+    C_01 = rec31(low_i, mid_i, mid_j + 1, high_j, A, B, C)
+    C_10 = rec31(mid_i + 1, high_i, low_j, mid_j, A, B, C)
+    C_11 = rec31(mid_i + 1, high_i, mid_j + 1, high_j, A, B, C)
+    return merge(C_00, C_01, C_10, C_11)
+
+
+            
+def rec69(low_i, high_i, low_j, high_j, A, B, C):
+    if low_i == high_i and low_j == high_j:
+        C[low_i][low_j] = A[low_i][low_j] * B[low_i][low_j]
+    else:
+        mid_i = (low_i + high_i) // 2
+        mid_j = (low_j + high_j) // 2
+        rec69(low_i, mid_i, low_j, mid_j, A, B, C)
+        rec69(low_i, mid_i, mid_j+1, high_j, A, B, C)
+        rec69(mid_i+1, high_i, low_j, mid_j, A, B, C)
+        rec69(mid_i+1, high_i, mid_j+1, high_j, A, B, C)
+
+
 offset_A = 0
 offset_B = N * N
 offset_C = 2 * N * N
 
 A1 = np.zeros([N, N])
 B1 = np.zeros([N, N])
-# store A and B in a row major way
+# store A1 and B1 in a row major way
 for i in range(N):
     for j in range(N):
         cs1.store(offset_A + i * N + j)
@@ -67,27 +198,13 @@ for i in range(N):
 
 A2 = np.zeros([N, N])
 B2 = np.zeros([N, N])
-# load values in rnd_vals1 and rnd_vals2 into A2 and B2, A2 and B2 are the block version of A1 and B1. The order of the values in A2 and B2 is different from A1 and B1, which is row major.
-# load rnd_val1 and rnd_val2 into A2 and B2. Each of them are N*N matrices.
-# A2 and B2 are block matrices, each block is K*K
-# A2 and B2 are stored in a block major way
-# store the values from rnd_val1 and rnd_val2 into A2 and B2, each of them are N*N matrices.
-
-elem = 0
-for block_i in range(N // K):
-    for block_j in range(N // K):
-        for within_block_i in range(K):
-            for within_block_j in range(K):
-                index_i = within_block_i + block_i * K
-                index_j = within_block_j + block_j * K
-                normal_i = elem // N
-                normal_j = elem % N
-                cs2.store(offset_A + index_i * N + index_j)
-                cs2.store(offset_B + index_i * N + index_j)
-                A2[normal_i][normal_j] = rnd_vals1[index_i][index_j]
-                B2[normal_i][normal_j] = rnd_vals2[index_i][index_j]
-                elem += 1
-
+for i in range(N):
+    for j in range(N):
+        (normal_i, normal_j) = convert_index(i, j, N, K)
+        cs2.store(offset_A + normal_i * N + normal_j)
+        cs2.store(offset_B + normal_i * N + normal_j)
+        A2[normal_i][normal_j] = rnd_vals1[i][j]
+        B2[normal_i][normal_j] = rnd_vals2[i][j]
 
 if algorithm == 'simple':
     C1 = np.zeros([N, N])
@@ -101,47 +218,59 @@ if algorithm == 'simple':
                 cs1.load(offset_B + k * N + j)      # load B[k][j]
                 cs1.store(offset_C + i * N + j)     # store C[i][j]
                 # write solution into C1 matrix
-                C1[i][j] = C1[i][j] + rnd_vals1[i][k] * rnd_vals2[k][j]
+                C1[i][j] = C1[i][j] + A1[i][k] * B1[k][j]
     print("row major arrays:")
     print(A1)
     print(B1)
     print(C1, end="\n\n")
-
     
     C2 = np.zeros([N, N])
     # block array
     for i in range(N):
         for j in range(N):
             for k in range(N):
-                # access with offset
-                cs2.load(offset_C + i * N + j)      # load C[i][j]
-                cs2.load(offset_A + i * N + k)      # load A[i][k]
-                cs2.load(offset_B + k * N + j)      # load B[k][j]
-                cs2.store(offset_C + i * N + j)     # store C[i][j]
-                # write solution into C2 matrix
-                C2[i][j] = C2[i][j] + rnd_vals1[i][k] * rnd_vals2[k][j]
+                (normal_i, normal_j) = convert_index(i, j, N, K)
+                (normal_i2, normal_j2) = convert_index(i, k, N, K)
+                (normal_i3, normal_j3) = convert_index(k, j, N, K)
+                cs2.load(offset_C + normal_i * N + normal_j)
+                cs2.load(offset_A + normal_i2 * N + normal_j2)
+                cs2.load(offset_B + normal_i3 * N + normal_j3)
+                cs2.store(offset_C + normal_i * N + normal_j)
+                C2[normal_i][normal_j] = C2[normal_i][normal_j] + A2[normal_i2][normal_j2] * B2[normal_i3][normal_j3]
     print("block arrays:")
     print(A2)
     print(B2)
     print(C2, end="\n\n")
 
 elif algorithm == 'recursive':
-    # recursive block matrix multiplication with matrix size N and block size K
-    def recursive_block_matrix_multiplication(A, B, C, N, K):
-        if N == K:
-            for i in range(N):
-                for j in range(N):
-                    for k in range(N):
-                        cs2.load(i * N + j)
-                        cs2.load(i * N + k)
-                        cs2.load(k * N + j)
-                        cs2.store(i * N + j)
-        else:
-            recursive_block_matrix_multiplication(A, B, C, N // 2, K)
-            recursive_block_matrix_multiplication(A, B, C, N // 2, K)
-            recursive_block_matrix_multiplication(A, B, C, N // 2, K)
-            recursive_block_matrix_multiplication(A, B, C, N // 2, K)
-    recursive_block_matrix_multiplication(rnd_vals1, rnd_vals2, np.zeros([N, N]), N, K)
+    # C1 = np.zeros([N, N])
+    # # row-major array
+    # for i in range(N):
+    #     for j in range(N):
+    #         for k in range(N):
+    #             # access with offset
+    #             cs1.load(offset_C + i * N + j)      # load C[i][j]
+    #             cs1.load(offset_A + i * N + k)      # load A[i][k]
+    #             cs1.load(offset_B + k * N + j)      # load B[k][j]
+    #             cs1.store(offset_C + i * N + j)     # store C[i][j]
+    #             # write solution into C1 matrix
+    #             C1[i][j] = C1[i][j] + A1[i][k] * B1[k][j]
+    # print("row major arrays:")
+    # print(C1, end="\n\n")
+
+    C1 = np.zeros([N, N])
+    C1 = rec31(0, N - 1, 0, N - 1, A1, B1, C1)
+    print("row major arrays:")
+    print(A1)
+    print(B1)
+    print(C1, end="\n\n")
+
+    C2 = np.zeros([N, N])
+    # rec32(0, N - 1, 0, N - 1, A2, B2, C2)
+    print("block arrays:")
+    print(A2)
+    print(B2)
+    print(C2, end="\n\n")
 
 
 # WRITE YOUR CODE ABOVE #
--- block-matrix/test.py
+++ block-matrix/test.py
@@ -1,9 +1,32 @@
 N = 8
 elem = 0
+K = 4
+
+def convert_index(index_i, index_j, N, K) -> tuple: 
+    # convert row-major index to block-major index
+    # block major index is the index of the element in the block-major array
+    elem = index_i * N + index_j
+    block = elem // (K * K)
+    block_i = block // (N // K)
+    block_j = block % (N // K)
+    within_block = elem % (K * K)
+    within_block_i = within_block // K
+    within_block_j = within_block % K
+    normal_i = within_block_i + block_i * K
+    normal_j = within_block_j + block_j * K
+    return (normal_i, normal_j)
+
+for i in range(N):
+    for j in range(N):
+        print(f"({convert_index(i, j, N, K)})", end="\t")
+    print()
+
+
 for i in range(N):
     for j in range(N):
         # print index with elem
-        print(f"[{elem} : ({i},{j})]", end="\t")
+        print(f"[{elem} : ({i},{j}), ({convert_index(i, j, N, K)})]", end="\t")
+        # print(f"[{elem} : ({convert_index(i, j, N, K)})]", end="\t")
         elem += 1
     print()
 
@@ -27,7 +50,3 @@ for block_i in range(n // k):
         print()
 
     
-    
-        
-    
-        
